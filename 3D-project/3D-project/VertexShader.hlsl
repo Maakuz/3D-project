ac931c8d048af47a6ea80a504ab340d79d8b2f17
Maakuz <markuswerdien@hotmail.com>
2017-01-07 15:50:03 +0100

    Reduced size of project

--- 3D-project/3D-project/VertexShader.hlsl
+++ 3D-project/3D-project/VertexShader.hlsl
@@ -10,12 +10,23 @@ struct VS_OUT
     float3 color : COLOR;
 };
 
+cbuffer WVPMatrixBuffer
+{
+    float4x4 world;
+    float4x4 view;
+    float4x4 projection;
+};
+
 VS_OUT main(VS_IN input)
 {
 	VS_OUT output = (VS_OUT)0;
 
     output.pos = float4(input.pos, 1);
     output.color = input.color;
+
+    output.pos = mul(output.pos, world);
+    output.pos = mul(output.pos, view);
+    output.pos = mul(output.pos, projection);
 
     return output;
 }
