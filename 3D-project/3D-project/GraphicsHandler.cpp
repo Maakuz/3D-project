bb1e425ee5702b5e8d7c876ab9e3561952e0a4e3
Maakuz <markuswerdien@hotmail.com>
2017-01-07 13:30:13 +0100

    Revert "added directxtk"
    This reverts commit e6ba5d2efca8744c6594022bdd2128db88ce1aad.

--- 3D-project/3D-project/GraphicsHandler.cpp
+++ 3D-project/3D-project/GraphicsHandler.cpp
@@ -117,7 +117,7 @@ void GraphicsHandler::createShaders()
 		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
 		{ "COLOR", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 }
 	};
-	
+
 	hr = this->gDevice->CreateInputLayout(inputDesc, ARRAYSIZE(inputDesc), vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), &this->vertexLayout);
 
 	if (FAILED(hr))
